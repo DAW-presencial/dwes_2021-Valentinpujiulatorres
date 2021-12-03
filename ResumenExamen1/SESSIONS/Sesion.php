7bdf478ec68f8dba46653f6e63ef2f3bfbe1d202
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-12-03 17:18:03 +0100

    Last commit Form

--- ResumenExamen1/SESSIONS/Sesion.php
+++ ResumenExamen1/SESSIONS/Sesion.php
@@ -10,14 +10,3 @@ if (isset($_COOKIE['CookieSesion'])) {
   setcookie('CookieSesion', 1, time() + 1200);
   echo "Has visitado esta página 1 vez";
 }
-//$_COOKIE['goodCookie'] = isset($_COOKIE['goodCookie']) ? ++$_COOKIE['niceCookie'] : 1;
-//echo "Has visitado esta página " . $_COOKIE['goodCookie'] . " veces";
-
-//if (isset($_GET['delete'])) {
-//  if (isset ($_COOKIE[session_name()])) {
-//    setcookie(session_name(), "", time() - 3600, "/");
-//    unset($_COOKIE[session_name()]);
-//  }
-//  session_unset(); // o bien $_SESSION = array();
-//  session_destroy();
-//}  

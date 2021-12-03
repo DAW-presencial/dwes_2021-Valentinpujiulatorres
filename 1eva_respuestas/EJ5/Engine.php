8fc0b7b98468f18fc8632f5cd8d72169444e4918
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-12-03 16:26:56 +0100

    Commit con todos los cambios

--- 1eva_respuestas/EJ5/Engine.php
+++ 1eva_respuestas/EJ5/Engine.php
@@ -1,14 +1,30 @@
 <?php 
 
+if (isset($_POST['Submit'])){
+    
+    echo "Nombre : " . $_POST['Name'] . " \n <br>";
+    echo "Apellidos : " . $_POST['Surname'] . "  \n <br>" ;
+    echo "Fecha : " . $_POST['Date'] . "  \n <br> ";
+ }
+
+
 $dir_subida = 'ARX_DB/';
 $fichero_subido = $dir_subida . basename($_FILES['fichero_usuario']['name']);
+$fichero_subido2 = $dir_subida . basename($_FILES['fichero_usuario2']['name']);
 
 echo '<pre>';
 if (move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido)) {
-    echo "El fichero es válido y se subió con éxito.\n";
+    echo "El fichero 1 es válido y se subió con éxito.\n";
 } else {
     echo "¡Posible ataque de subida de ficheros!\n";
 }
+echo '<pre>';
+if (move_uploaded_file($_FILES['fichero_usuario2']['tmp_name'], $fichero_subido2)) {
+    echo "El fichero 2 es válido y se subió con éxito.\n";
+} else {
+    echo "¡Posible ataque de subida de ficheros!\n";
+}
+
 
 echo 'Más información de depuración:';
 print_r($_FILES);
--- 1eva_respuestas/EJ5/formularioEnvio.php
+++ 1eva_respuestas/EJ5/formularioEnvio.php
@@ -1,11 +1,24 @@
 <html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Examen 1 Evaluación Valentín Pujiula</title>
+</head>
     <body>
-    <form enctype="multipart/form-data" action="Engine.php" method="POST">
+    <form enctype="multipart/form-data" action="Engine.php" method="post">
+    <label for="Name">Introduce tu Nombre:</label>
+    <input type="text" name="Name"><br>
+    <label for="Surname">Introduce tus apellidos:</label>
+    <input type="text" name="Surname"><br>
+    <label for="Date">Fecha:</label>
+    <input type="date" name="Date"><br>
     <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-
-    <label>Enviar este fichero:</label> 
-    <input name="fichero_usuario" type="file" />
-    <input type="submit" value="Enviar fichero" />
+    <label for="fichero_usuario">Enviar este fichero:</label> 
+    <input name="fichero_usuario" type="file" /><br>
+    <label for="fichero_usuario2">Enviar este fichero:</label> 
+    <input name="fichero_usuario2" type="file" /><br>
+    <input type="submit" value="Enviar fichero" name="Submit" />
 </form>
         <?php
 

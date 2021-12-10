02f02589cd78a7a007eb90bfd24fbc12adefe765
Valentinpujiulatorres <vpujiula@cifpfbmoll.eu>
2021-12-10 17:31:13 +0100

    Entrar

--- AgendaDatabase/Agenda_Pujiula_Torres_Valentin/Index.php
+++ AgendaDatabase/Agenda_Pujiula_Torres_Valentin/Index.php
@@ -27,7 +27,9 @@
     <div id="databaseConn">
         <?php
             require_once 'pdoconfig.php';
- 
+
+
+                // echo de las 2 formas , pdo y msqli , es irrelevante , la utilidad es la misma
             try {
                 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
                 echo "<p>Connected to $dbname at $host successfully.<p>";
@@ -45,10 +47,13 @@
         <input type="text" name="Surname"><br>
         <label for="Tel">Introduce tu Número de teléfono</label>
         <input type="number" name="Tel"><br>
-        <input type="submit" value="Enviar a DB" name="Submit" />
+        <input type="submit" value="Enviar a DB" name="Submit" /><br>
+        <input type="submit" value="Update Register" name="Update" /><br>
+        <input type="submit" value="Erase Reñgister" name="Erase" />
         </form>
         <br>
         <hr>
+        
         
 
     </div>
@@ -57,16 +62,22 @@
         require 'pdoconfig.php';
         $Connection = new mysqli("$host","$username","$password","$dbname");
 
+        
+
         if (isset($_POST['Submit'])){
     
             $Name = $_POST['Name'];
             $Surname = $_POST['Surname'];
             $Telephone = $_POST['Tel'];
+            $UpdateData = "UPDATE contactlist  SET Surname = '$Surname', Phone = '$Telephone' WHERE Name = '$Name';";
+            $RemoveData = "";
             $InsertData = "INSERT INTO contactlist (Name,Surname,Phone) VALUES ('$Name', '$Surname', '$Telephone')";
             if ( $Connection -> query($InsertData) === TRUE && $Name !== "") {
                 echo "<br><br><p style=\"color: green;\">New record created successfully<p>";
-              } else {
-                echo "Error: "  . "<br>" . $conn->error;
+            }
+               
+               else{
+                echo "Error: "  . "<br>" ;
               }
               $ShowMe =  $Connection -> query("SELECT * FROM contactlist");
             
@@ -75,7 +86,39 @@
                   echo " CONTACTO [  " . "Name: " . $fila['Name'] . ",  Surname: " .  $fila['Surname'] . ",  Phone: " . $fila['Phone'] . "]" ."<br>" ;
             
                 };
+        }else if (isset($_POST['Update'])){
+            $Name = $_POST['Name'];
+            $Surname = $_POST['Surname'];
+            $Telephone = $_POST['Tel'];
+            $UpdateData = "UPDATE contactlist  SET Surname = '$Surname', Phone = '$Telephone' WHERE Name = '$Name';";
+            if ( $Connection -> query($UpdateData) === TRUE && $Name !== "") {
+                echo "<br><br><p style=\"color: green;\">New Update successfully<p>";
+                $ShowMe =  $Connection -> query("SELECT * FROM contactlist");
+            
+                $ShowMe->data_seek(0);
+                while ($fila = $ShowMe->fetch_assoc()) {
+                    echo " CONTACTO [  " . "Name: " . $fila['Name'] . ",  Surname: " .  $fila['Surname'] . ",  Phone: " . $fila['Phone'] . "]" ."<br>" ;
+              
+                  };
+            }
+
+        }else if (isset($_POST['Erase'])){
+            $Name = $_POST['Name'];
+            $EraseData = "DELETE FROM contactlist WHERE Name = '$Name';";
+            if ( $Connection -> query($EraseData) === TRUE && $Name !== "") {
+                echo "<br><br><p style=\"color: green;\">New erase succesfully</p>";
+
+            }
+            $ShowMe =  $Connection -> query("SELECT * FROM contactlist");
+            
+            $ShowMe->data_seek(0);
+            while ($fila = $ShowMe->fetch_assoc()) {
+                echo " CONTACTO [  " . "Name: " . $fila['Name'] . ",  Surname: " .  $fila['Surname'] . ",  Phone: " . $fila['Phone'] . "]" ."<br>" ;
+          
+              };
+
         }
+        
              ?>
            
          
